aa24773b2d584a02bd7dbb757dfcee6b4df23cf2
UncleSamTech <samueloel3486@gmail.com>
2023-06-20 22:07:51 -0600

    added two functions and a class

--- scratch_parser.py
+++ scratch_parser.py
@@ -14,6 +14,7 @@ class scratch_processor:
         self.second_value_list = []
         self.dict_key_list = []
         self.dict_value_list = []
+        self.json_data = ""
 
     
     def extract_dict_values(self,value):
@@ -22,7 +23,6 @@ class scratch_processor:
                 print(keys , '->', values)
                 self.dict_key_list.append(keys)
                 self.dict_value_list.append(values)
-        #return self.dict_key_list, self.dict_value_list
 
     def extract_list_values(self, values):
         if isinstance(values, list) and len(values) > 0:
@@ -33,17 +33,13 @@ class scratch_processor:
 
     def parse_json(self,file_path):
         string_to_parse = Path(file_path).read_text()
-        data = json.loads(string_to_parse) 
-        dict_keys = []
-        dict_value = []
-        self.top_keys_list = data.keys()
+        self.json_data = json.loads(string_to_parse) 
+        self.top_keys_list = self.json_data.keys()
         for i in self.top_keys_list:
-            if len(data[i]) > 0:
-                for j in data[i]:
+            if len(self.json_data[i]) > 0:
+                for j in self.json_data[i]:
                     if isinstance(j,dict):
                         for keys,value in j.items():
-                            is_dict = 'a dictionary' if isinstance(value,dict) else 'not a dictionary'
-                        #print(keys , '->' , value, type(value))
                             self.second_keys_list.append(keys)
                             self.second_value_list.append(value)
                             if type(value) is str:
@@ -51,12 +47,10 @@ class scratch_processor:
                             elif type(value) is int:
                                 print(keys , 'is an int datatype with ', value)
                             elif type(value) is dict:
-                                #print(keys , ' is a dictionary datatype', value)
                                 self.extract_dict_values(value)
                             elif type(value) is bool:
                                 print(keys, 'is a bool datatype with value', value)
                             elif type(value) is list:
-                                #print(keys, 'is a list with length of ', len(value))
                                 self.extract_list_values(value)
                             else:
                                 print('unknown data structure')
