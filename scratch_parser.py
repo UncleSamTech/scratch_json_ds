12a6550df0e82ff954b3e1a6e8beb58175167d58
UncleSamTech <samueloel3486@gmail.com>
2023-06-20 21:24:38 -0600

    wip

--- scratch_parser.py
+++ scratch_parser.py
@@ -15,9 +15,24 @@ def parse_json(file_path):
             for j in data[i]:
                 if isinstance(j,dict):
                     for keys,value in j.items():
-                        print(keys , '->' , value)
+                        is_dict = 'a dictionary' if isinstance(value,dict) else 'not a dictionary'
+                        #print(keys , '->' , value, type(value))
                         dict_keys.append(keys)
                         dict_value.append(value)
+                        if(type(value) is str):
+                            print(keys ,'is string datatype',value)
+                        elif(type(value) is int):
+                            print(keys , 'is an int datatype', value)
+                        elif type(value) is dict:
+                            print(keys , ' is a dictionary datatype', value)
+                        elif type(value) is bool:
+                            print(keys, 'is a bool datatype', value)
+                        elif type(value) is list:
+                            print(keys, 'is a list with length of ', len(value))
+                        else:
+                            print('unknown data structure')
+
+                        
 
 
 parse_json('json_files/actual_response.json')
